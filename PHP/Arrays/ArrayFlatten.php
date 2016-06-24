0d10e8a246db1fcd71436efee7ba2408e8e1f4f7
hardikshah7 <shahhardik2013@gmail.com>
2016-06-24 16:02:48 -0400

    Update ArrayFlatten.php

--- PHP/Arrays/ArrayFlatten.php
+++ PHP/Arrays/ArrayFlatten.php
@@ -18,9 +18,22 @@
  */
 
 function flattenArray($array) {
+  if(is_null($array) || empty($array)) {
+		return array(-1);
+	}
+	$resultarray = [];
+	foreach ($array as $element) {
+		if (is_array($element)) {
+			$resultarray = array_merge($resultarray, flattenArray($element));
+		}
+		else {
+			array_push ($resultarray, $element);
+		}		
+	}
+	return $resultarray;
 
 }
 
 $array = [1,2,3,[4,5,6],[7,[8,9]]];
 
-print flattenArray($array);
+print (implode(flattenArray($array)));
