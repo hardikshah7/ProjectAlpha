27a411eec5d14a27839716654117db0b9c574d64
hardikshah7 <shahhardik2013@gmail.com>
2016-06-29 19:32:35 -0400

    Solved Collections problem

--- PHP/Collections/ActualCollection.php
+++ PHP/Collections/ActualCollection.php
@@ -10,10 +10,79 @@ require_once 'iCollection.php';
 
 class ActualCollection implements Collection
 {
+	public $mycollection = [];
 
+	public function addItem($item,$key=false,$callback = null) {
+		if ($key) {
+			$this->mycollection[$key] = $item;
+			if($callback){
+				$callback();
+			}
+			return true;	
+		}
+		else {
+			$this->mycollection[] = $item;
+			return true;
+		}
+		return false;
+	}
+
+	public function delItem($key,$callback = null) {
+		if(isset($this->mycollection[$key])){
+			unset($this->mycollection[$key]);
+			if($callback) {
+				$callback();
+			}
+			return true;
+		}
+		return false;
+	}
+
+	public function getItem($key) {
+		return $this->mycollection[$key];
+	}
+
+	public function length() {
+		return count($this->mycollection);
+	}
+
+	public function count() {
+		return count($this->mycollection);
+	}
+	
+	public function clear() {
+		$this->mycollection = [];
+	}
+
+	public function hasKey($key) {
+		return array_key_exists($key, $this->mycollection);
+	}
+
+	public function keys() {
+		return array_keys($this->mycollection);
+	}
 }
 
 class ActualSortedCollection extends ActualCollection implements SortedCollection {
+public $sortingorder = null;
 
+	public function setSort($sort = SortedCollection::SORT_ASC) {
+		if($sort) {
+			$this->sortingorder = 1;
+		}
+		else {
+			$this->sortingorder = 0;
+		}
+	}
+	
+	public static function fromUnsorted(Collection $collection, $sort) {
+		if($sort) {
+			krsort($collection);
+		}
+		else if($sort == 0) {
+			ksort($collection);
+		}
+		return $collection;
+	}
 
 }
